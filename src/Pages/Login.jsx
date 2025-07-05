e3cefd9a1a3c00146ea98c7af9aa4c081efd0d46
kizito-1 <kizitochidubem01@gmail.com>
2025-07-06 00:22:36 +0100

    working on the project

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Login = () => {
+ let [count, setCount] = useState(0)
+   
+  useEffect(() => {
+    console.log(`You clicked me ${count} times`)
+  }, [count])
+
   return (
-    <div>Login</div>
+    <div>
+      <button onClick={() => setCount(count++)}>click me</button>
+    </div>
   )
 }
 

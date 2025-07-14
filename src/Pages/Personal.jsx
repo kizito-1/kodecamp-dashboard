418008c9bff065edd592255eb8d746df9942a285
kizito-1 <kizitochidubem01@gmail.com>
2025-07-14 17:26:53 +0100

    updating the project

--- src/Pages/Personal.jsx
+++ src/Pages/Personal.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { IoCloseSharp} from "react-icons/io5";
+import "../styles/personal.css";
 
 const Personal = () => {
   return (
-    <div>Personal</div>
-  )
-}
+    <>
+      <section className="personal-section">
+        <div className="personal-container">
+          <div className="header">
+            <div className="header-texts">
+              <h2>Personal Information</h2>
+              <p>2 of 3</p>
+            </div>
 
-export default Personal
+            <div className="close">
+              <IoCloseSharp size={25} />
+            </div>
+          </div>
+
+          <form>
+            <div className="name-input">
+              <input type="text" placeholder="Full Name" />
+            </div>
+
+            <div className="checkbox">
+              <p>Gender:</p>
+              <input type="checkbox" name="" id="" />
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Personal;
